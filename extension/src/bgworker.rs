c7e431f6f0b1da4395479139b24fc90cde364e62
Jonathan Pickard <jpickard@analyzer1.com>
2024-06-03 14:24:11 -0400

    Checkpoint

--- extension/src/bgworker.rs
+++ extension/src/bgworker.rs
@@ -18,24 +18,10 @@ pub extern "C" fn _PG_init() {
 #[pg_guard]
 #[no_mangle]
 pub extern "C" fn background_worker_main(arg: pg_sys::Datum) {
-    // let arg = unsafe { i32::from_polymorphic_datum(arg, false, pg_sys::INT4OID) };
 
-    // these are the signals we want to receive.  If we don't attach the SIGTERM handler, then
-    // we'll never be able to exit via an external notification
     BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGHUP | SignalWakeFlags::SIGTERM);
-
-    // we want to be able to use SPI against the specified database (postgres), as the superuser which
-    // did the initdb. You can specify a specific user with Some("my_user")
-    // Q: Does this default to pg_auto_dw
     BackgroundWorker::connect_worker_to_spi(Some("pg_auto_dw"), None);
 
-    // log!(
-    //     "Hello from inside the {} BGWorker!  Argument value={}",
-    //     BackgroundWorker::get_name(),
-    //     arg.unwrap()
-    // );
-    // log!("Client BG Worker - wait 10secs.");
-    // sleep(Duration::new(10, 0));
     while BackgroundWorker::wait_latch(Some(Duration::from_secs(10))) {
         let result: Result<(), pgrx::spi::Error> = BackgroundWorker::transaction(|| {
             Spi::connect(|mut client| {
@@ -65,7 +51,7 @@ pub extern "C" fn background_worker_main(arg: pg_sys::Datum) {
                         }
                     },
                     Err(e) => {
-                        log!("Error checking schema AUTO_DW: {:?}", e);
+                        log!("Error checking TABLE AUTO_DW.SOURCE_OJBECTS: {:?}", e);
                     }
                 }
                 Ok(())
