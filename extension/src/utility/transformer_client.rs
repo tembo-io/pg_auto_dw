b04776b7943895fa01b3beca9dc983876fc0cf7e
Jonathan Pickard <jpickard@analyzer1.com>
2024-09-27 08:35:41 -0400

    Refactor TransformerServerType with FromStr implementation - Implemented FromStr for TransformerServerType to simplify string parsing. - Improved error handling by returning &'static str for invalid server types.

--- extension/src/utility/transformer_client.rs
+++ extension/src/utility/transformer_client.rs
@@ -1,28 +1,35 @@
 use crate::model::prompt_template::PromptTemplate;
 use super::{guc, openai_client, ollama_client};
 use TransformerServerType::{OpenAI, Ollama};
+use std::str::FromStr;
 
 pub enum TransformerServerType {
     OpenAI,
     Ollama
 }
 
-pub async fn send_request(new_json: &str, template_type: PromptTemplate, col: &u32, hints: &str) 
-    -> Result<serde_json::Value, Box<dyn std::error::Error>>  {
+impl FromStr for TransformerServerType {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<TransformerServerType, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "openai" => Ok(OpenAI),
+            "ollama" => Ok(Ollama),
+            _ => Err("Invalid Transformer Server Type"),
+        }
+    }
+}
+
+pub async fn send_request(new_json: &str, template_type: PromptTemplate, col: &u32, hints: &str)  -> Result<serde_json::Value, Box<dyn std::error::Error>>  {
     
-        let transformer_server_type = guc::get_guc(guc::PgAutoDWGuc::TransformerServerType).ok_or("GUC: Transformer Server Type is not set.")?;
-        let transformer_server_type = {
-            if transformer_server_type == "openai".to_string() {
-                TransformerServerType::OpenAI
-            } else {
-                TransformerServerType::Ollama
-            }
-        };
+        let transformer_server_type_str = guc::get_guc(guc::PgAutoDWGuc::TransformerServerType).ok_or("GUC: Transformer Server Type is not set.")?;
+
+        let transformer_server_type = transformer_server_type_str.parse::<TransformerServerType>()
+            .map_err(|e| format!("Error parsing Transformer Server Type: {}", e))?;
 
     match transformer_server_type {
-        OpenAI => {openai_client::send_request(new_json, template_type, col, hints).await},
-        Ollama => {ollama_client::send_request(new_json, template_type, col, hints).await},
+        OpenAI => openai_client::send_request(new_json, template_type, col, hints).await,
+        Ollama => ollama_client::send_request(new_json, template_type, col, hints).await,
     }
-    
 }
 
